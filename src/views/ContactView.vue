1cdf0b23236003150a4394966763cc1c678f0f12
Sanctus <jhn.hines@gmail.com>
2025-03-26 23:00:58 -0500

    Contact form body created and semi stylized

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -8,23 +8,39 @@
     <!-- container class for positioning the content in the middle and a litle bit down from the top -->
     <div class="container m-auto max-w-2xl py-16">
       <!-- this div is for the forms outter body -->
-      <div class="div">
-        <h1 class="text-center">Let's Get In Touch</h1>
+      <div class="div ">
+        <h1 class="text-center text-6xl font-semibold mb-6">Let's Get In Touch</h1>
         <form>
             <!-- This div is for sizing the inputs and arranging the form items -->
-            <div class="div">
-              <div>
+            <div class="form flex flex-col items-center px-6 py-8 mb-4 border rounded-md m-4">
+              <div class="mb-4">
                 <label class="block" for="name">Name</label>
-                <input type="text" name="name" id="name">
-
-
+                <input class="w-80 border-2 rounded" type="text" name="name" id="name" placeholder="John Doe" required>
               </div>
           
+              <div class="mb-4">
+                <label class="block" for="name">Email</label>
+                <input class="w-80 border-2 rounded" type="email" name="name" id="name" placeholder="email@example.com">
+              </div>
+
+              <div class="mb-4">
+                <label class="block" for="name">Message</label>
+                <textarea 
+                class="w-80 border-2 rounded"
+                rows="5"
+                cols="33"
+                name="message" 
+                id="message" 
+                maxlength="1000" placeholder= "Enter your message" required/>
+              </div>
               
-              <div>
-                <button
-                  class="font-" 
-                  type="submit">Send</button>
+              <div class="justify-self-center ">
+                <button class="submit-button" type="submit">
+                  <span class="flex items-center gap-2">
+                    Submit
+                    <i class="pi pi-send"></i>
+                  </span>
+                </button>
               </div>
             </div>
           </form>
@@ -39,11 +55,31 @@
 </template>
 
 <style scoped>
- * {
-  color: white;
- }
 
- button {
+  h1, label {
+    color: white;
+  }
+
+  .form {
+    background-color: var(--j-darkblue);
+  }
+
+  .div, .form {
+    border-color: var(--j-yellow);
+  }
+
+  input, textarea {
+    border-color: var(--j-claret);
+    padding: 4px;
+    box-sizing:border-box;
+  }
+
+  .pi {
+    color: black;
+  }
+
+
+  .submit-button {
     background-color: var(--j-yellow);
     color: black;
     padding-top: .5rem;
@@ -53,8 +89,9 @@
     border-radius: 0.375rem;
     font-weight: bold;
     font-size: large;
- }
- button:hover {
+  }
+
+  .submit-button:hover {
     background-color: var(--j-claret);
     color: white;
     padding-top: .5rem;
@@ -64,5 +101,8 @@
     border-radius: 0.375rem;
     font-weight: bold;
     font-size: large;
- }
+  }
+  .submit-button:hover .pi {
+    color:white
+  }
 </style>
