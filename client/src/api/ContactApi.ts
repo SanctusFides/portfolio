7fe04b0c2c6435a65149029ea3f1876e8d304d90
Sanctus <jhn.hines@gmail.com>
2025-04-10 14:33:44 -0500

    not seeing change reflecting in behavior

--- client/src/api/ContactApi.ts
+++ client/src/api/ContactApi.ts
@@ -36,10 +36,12 @@ ${formData.message}
 
 // Need to validate the form details incase anything funky was changed on the source to remove the required checks
 export async function contactHandler(formData: FormData) {
-  console.log(api_url);
+  console.log("inside contact handler, about to print url...");
   
-  // axios.post('http://localhost:5000/api/email', formData);
-  // axios.post(`${api_url}`, formatEmail(formData));  
+  console.log(api_url);
 }
+
+// axios.post('http://localhost:5000/api/email', formData);
+// axios.post(`${api_url}`, formatEmail(formData));  
 
  
