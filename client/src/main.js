88d9c034c8ab650fe1a573d205d5426a17fba688
Sanctus <jhn.hines@gmail.com>
2025-04-15 13:34:08 -0500

    Apparently setting cors to origin true only is allowed if using, express which is what I am trying to avoid by offloading to AWS lambda

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,6 @@ import 'primeicons/primeicons.css';
 import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
 import router from './router/index.js';
-import cors from 'cors';
 
 import { createApp } from 'vue';
 import App from './App.vue';
@@ -12,6 +11,5 @@ import App from './App.vue';
 const app = createApp(App)
 app.use(router);
 app.use(Toast);
-app.use(cors({ origin: true }));
 
 app.mount('#app');
