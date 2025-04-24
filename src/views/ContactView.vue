3e71a9c9c2d585346f843c6a344636f756b3b379
Sanctus <jhn.hines@gmail.com>
2025-04-24 17:41:50 -0500

    Fixed footer anchor in mobile format and cleaned up CSS

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -5,18 +5,23 @@
 </script>
 
 <template>
-  <Navbar />
-
-  <!-- <section class="content bg-[#002642] h-screen flex flex-col items-center justify-items-center"> -->
-  <section class="content font-manrope bg-jblue h-screen">
-    <!-- container class for positioning the content in the middle and a litle bit down from the top -->
-    <div class="container m-auto max-w-2xl py-16">
-      <!-- this div is for the forms outter body -->
-      <div class="div">
-        <h1 class="text-center text-6xl font-semibold mb-6 text-white">Let's Get In Touch</h1>
-        <ContactForm />
+  <div class="flex flex-col h-dvh">    
+    <Navbar />
+    <!-- <section class="content bg-[#002642] h-screen flex flex-col items-center justify-items-center"> -->
+    <section class="content font-manrope bg-jblue">
+      <!-- container class for positioning the content in the middle and a litle bit down from the top -->
+      <div class="container m-auto max-w-2xl pt-8 sm:pt-16">
+        <!-- this div is for the forms outter body -->
+        <div class="div">
+          <h1 class="text-center text-6xl font-semibold mb-6 text-white">Let's Get In Touch</h1>
+          <ContactForm />
+        </div>
       </div>
-    </div>
-  </section>
-  <Footer />
+    </section>
+    <Footer />
+  </div>
 </template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,20 +8,26 @@
 </script>
 
 <template>
-  <Navbar />
-  <section class="@container bg-jblue h-screen font-manrope text-white flex">
-    <div class="flex flex-col @sm:flex-row gap-8 sm:gap-0 w-full sm:h-full">
-      <div class="flex flex-col justify-center items-center sm:flex-1 gap-3 sm:gap-6">
-        <h1 class="text-6xl sm:text-9xl text-jyellow">John Hines</h1>
-        <p class="text-2xl sm:text-4xl text-center">Junior Software Engineer</p>
-        <p class="text-xl sm:text-2xl">Full-Stack Developer</p>
+  <div class="content flex flex-col h-screen">
+    <Navbar />
+    <section class="@container grow-1 font-manrope text-white">
+      <div class="flex flex-col @sm:flex-row gap-8 sm:gap-0 pt-4 sm:-pt-0 w-full sm:h-full">
+        <div class="flex flex-col justify-center items-center sm:flex-1 gap-3 sm:gap-6">
+          <h1 class="text-6xl sm:text-9xl text-jyellow">John Hines</h1>
+          <p class="text-2xl sm:text-4xl text-center">Junior Software Engineer</p>
+          <p class="text-xl sm:text-2xl">Full-Stack Developer</p>
+        </div>
+        <div class="flex flex-col gap-8 sm:justify-center items-center flex-1">
+          <img class="rounded-full h-64 sm:h-96" :src="image" alt="image of John Hines" />
+          <ResumeButton />
+          <ContactButton />
+        </div>
       </div>
-      <div class="flex flex-col gap-8 sm:justify-center items-center flex-1">
-        <img class="rounded-full h-64 sm:h-96" :src="image" alt="image of John Hines" />
-        <ResumeButton />
-        <ContactButton />
-      </div>
-    </div>
-  </section>
-  <Footer  />
+    </section>
+    <Footer />
+  </div>
 </template>
+
+<style scoped>
+
+</style>
