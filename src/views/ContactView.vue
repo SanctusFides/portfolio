7a98f2ca2b105868865095844257882e730098a1
Sanctus <jhn.hines@gmail.com>
2025-04-24 18:51:08 -0500

    Seeing if centering changes anything

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -8,9 +8,9 @@
   <div class="flex flex-col h-dvh">    
     <Navbar />
     <!-- <section class="content bg-[#002642] h-screen flex flex-col items-center justify-items-center"> -->
-    <section class="font-manrope bg-jblue mb-auto">
+    <section class="content font-manrope bg-jblue">
       <!-- container class for positioning the content in the middle and a litle bit down from the top -->
-      <div class="container m-auto max-w-2xl pt-8 sm:pt-16">
+      <div class="container m-auto max-w-2xl pt-8 md:pt-16">
         <!-- this div is for the forms outter body -->
         <div class="div">
           <h1 class="text-center text-6xl font-semibold mb-6 text-white">Let's Get In Touch</h1>
