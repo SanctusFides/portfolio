d35986ce9ddbb16a0f0b72b05d1defa4c535953d
Sanctus <jhn.hines@gmail.com>
2025-04-24 16:03:37 -0500

    Added image element to homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,20 +3,21 @@
   import Footer from '../components/Footer.vue';
   import ResumeButton from '../components/ResumeButton.vue';  
   import ContactButton from '../components/ContactButton.vue';
+  import image from '@/assets/img/john.jpg';
 
 </script>
 
 <template>
   <Navbar />
   <section class="@container bg-jblue h-screen font-manrope text-white flex">
-    <div class="flex flex-col @sm:flex-row w-full sm:h-full">
-      <div class="flex flex-col justify-center items-center flex-1 gap-3 sm:gap-6">
+    <div class="flex flex-col @sm:flex-row gap-8 sm:gap-0 w-full sm:h-full">
+      <div class="flex flex-col justify-center items-center sm:flex-1 gap-3 sm:gap-6">
         <h1 class="text-6xl sm:text-9xl text-jyellow">John Hines</h1>
         <p class="text-2xl sm:text-4xl text-center">Junior Software Engineer</p>
         <p class="text-xl sm:text-2xl">Full-Stack Developer</p>
       </div>
       <div class="flex flex-col gap-8 sm:justify-center items-center flex-1">
-
+        <img class="rounded-full h-64 sm:h-96" :src="image" alt="image of John Hines" />
         <ResumeButton />
         <ContactButton />
       </div>
