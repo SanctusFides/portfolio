ffe74660164689d25d06e3b278dad8a19e832c82
Sanctus <jhn.hines@gmail.com>
2025-04-24 18:28:15 -0500

    Need to test on actual phone

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import { RouterView } from 'vue-router';
+  import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <RouterView />
+  <Footer />
 </template>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,21 +10,21 @@
 <template>
   <div class="content flex flex-col h-screen">
     <Navbar />
-    <section class="@container grow-1 font-manrope text-white">
-      <div class="flex flex-col @sm:flex-row gap-8 sm:gap-0 pt-4 sm:-pt-0 w-full sm:h-full">
-        <div class="flex flex-col justify-center items-center sm:flex-1 gap-3 sm:gap-6">
-          <h1 class="text-6xl sm:text-9xl text-jyellow">John Hines</h1>
-          <p class="text-2xl sm:text-4xl text-center">Junior Software Engineer</p>
-          <p class="text-xl sm:text-2xl">Full-Stack Developer</p>
+    <section class="@container  font-manrope text-white">
+      <div class="flex flex-col @md:flex-row gap-8 md:gap-0 pt-4 md:-pt-0 w-full md:h-full">
+        <div class="flex flex-col justify-center items-center md:flex-1 gap-3 md:gap-6">
+          <h1 class="text-6xl md:text-9xl text-jyellow text-center">John Hines</h1>
+          <p class="text-2xl md:text-4xl text-center">Junior Software Engineer</p>
+          <p class="text-xl md:text-2xl">Full-Stack Developer</p>
         </div>
-        <div class="flex flex-col gap-8 sm:justify-center items-center flex-1">
-          <img class="rounded-full h-64 sm:h-96" :src="image" alt="image of John Hines" />
+        <div class="flex flex-col gap-8 md:justify-center items-center flex-1">
+          <img class="rounded-full h-64 md:h-96" :src="image" alt="image of John Hines" />
           <ResumeButton />
           <ContactButton />
         </div>
       </div>
     </section>
-    <Footer />
+    <!-- <Footer /> -->
   </div>
 </template>
 
