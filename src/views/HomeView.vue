04e775a1d74d72e63dc259e7dcfee14d57d5829f
Sanctus <jhn.hines@gmail.com>
2025-04-24 19:21:56 -0500

    testing LG change and if footer works on mobile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,21 +4,20 @@
   import ResumeButton from '../components/ResumeButton.vue';  
   import ContactButton from '../components/ContactButton.vue';
   import image from '@/assets/img/john.jpg';
-
 </script>
 
 <template>
-  <div class="content flex flex-col h-full">
+  <div class="content flex flex-col h-dvh">
     <Navbar />
     <section class="@container grow-1 font-manrope text-white">
-      <div class="flex flex-col @md:flex-row gap-8 md:gap-0 pt-4 md:-pt-0 w-full md:h-full">
-        <div class="flex flex-col justify-center items-center md:flex-1 gap-3 md:gap-6">
-          <h1 class="text-6xl md:text-9xl text-jyellow text-center">John Hines</h1>
-          <p class="text-2xl md:text-4xl text-center">Junior Software Engineer</p>
-          <p class="text-xl md:text-2xl">Full-Stack Developer</p>
+      <div class="flex flex-col @lg:flex-row gap-8 lg:gap-0 pt-4 lg:-pt-0 w-full lg:h-full">
+        <div class="flex flex-col justify-center items-center lg:flex-1 gap-3 lg:gap-6">
+          <h1 class="text-6xl lg:text-9xl text-jyellow text-center">John Hines</h1>
+          <p class="text-2xl lg:text-4xl text-center">Junior Software Engineer</p>
+          <p class="text-xl lg:text-2xl">Full-Stack Developer</p>
         </div>
-        <div class="flex flex-col gap-8 md:justify-center items-center flex-1">
-          <img class="rounded-full h-64 md:h-96" :src="image" alt="image of John Hines" />
+        <div class="flex flex-col gap-8 lg:justify-center items-center flex-1">
+          <img class="rounded-full h-64 lg:h-96" :src="image" alt="image of John Hines" />
           <ResumeButton />
           <ContactButton />
         </div>
