ef875c88cd87f2ff487fe5009dbb30c02b57bfb6
Sanctus <jhn.hines@gmail.com>
2025-05-12 15:26:27 -0500

    Updated Experience view to have proper ultrawide support. Also tweaked the ultrawide framing to pop in a bit sooner to avoid over strech of previous tier margins

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -9,8 +9,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <div class="content flex flex-col h-dvh">
     <Navbar />
-    <section class="@container font-manrope text-white grow-1 mx-auto w-full xl:max-w-3/4">
-      <div class="flex flex-col pt-4 lg:-pt-0 w-full lg:h-full px-4 pt-8 md:pt-16 ">
+    <!-- <section class="@container font-manrope text-white grow-1 mx-auto w-full xl:max-w-3/4"> -->
+    <section class="@container font-manrope text-white grow-1 py-4 lg:py-3">
+      <div class="flex flex-col pt-4 w-full lg:h-full px-4 lg:px-32 mx-auto 
+      @3xl:max-w-3/4 @8xl:max-w-2/4">
         <h1 class="text-5xl lg:text-5xl text-jyellow font-bold mb-4">Projects</h1>
         <div class="flex flex-col 2xl:flex-row gap-6">
           <FlavourAppBox /> 
